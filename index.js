6bf67dd25561652787a408159bf63cbcb35630a2
nilsmanc <etereaiberia@gmail.com>
2022-07-08 19:57:48 +0300

    mongoDB, express validator, bcrypt

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js';
+
+mongoose
+  .connect('mongodb+srv://admin:wwwwww@cluster0.ae06jup.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+app.post('/auth/register', registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
 
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: 'Имя Фамилия',
-    },
-    'secret123',
-  );
-  res.json({
-    success: true,
-    token,
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
