e58bab3fd3bab335aa75c3b9ce81f8cb62c82898
nilsmanc <etereaiberia@gmail.com>
2022-07-09 14:34:48 +0300

    authorization

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
+
 import { registerValidation } from './validations/auth.js';
-import UserModel from './models/User.js';
+
+import checkAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js';
 
 mongoose
-  .connect('mongodb+srv://admin:wwwwww@cluster0.ae06jup.mongodb.net/?retryWrites=true&w=majority')
+  .connect(
+    'mongodb+srv://admin:wwwwww@cluster0.ae06jup.mongodb.net/blog?retryWrites=true&w=majority',
+  )
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -15,27 +19,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+app.post('/auth/login', UserController.login);
 
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
+app.post('/auth/register', registerValidation, UserController.register);
 
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json(user);
-});
+app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
   if (err) {
